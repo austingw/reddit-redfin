aed8525ba322cb8fb25a2ca8a2fcb936ed689014
austingw <senortinman@gmail.com>
2023-08-23 20:09:19 -0400

    Refactor some of the comment hook for clarity

--- src/hooks/useFindComments.ts
+++ src/hooks/useFindComments.ts
@@ -2,17 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 //GET the comments for a given listing from the API
-export const findComments = async (listingId: string) => {
-  axios
-    .get("/api/getComments", {
+export const findComments = async (listingId: number) => {
+  try {
+    const response = await axios.get("/api/getComments", {
       params: {
-        id: listingId,
+        listingId,
       },
-    })
-    .then((res) => res?.data);
+    });
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching listings:", error);
+  }
 };
 
-const useFindComments = (listingId: string) => {
+const useFindComments = (listingId: number) => {
   return useQuery(["comments", listingId], () => findComments(listingId));
 };
 
