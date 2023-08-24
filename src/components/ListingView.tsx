e5e1d21a8101b9c4b0afc90c51456cfe7faf7055
austingw <senortinman@gmail.com>
2023-08-23 20:07:49 -0400

    Add listing view component

--- src/components/ListingView.tsx
+++ src/components/ListingView.tsx
@@ -1,9 +1,127 @@
-const ListingFullView = () => {
+import useCreateComment from "@/hooks/useCreateComment";
+import useFindComments from "@/hooks/useFindComments";
+import { Comment, CommentData } from "@/types/comments";
+import { Listing } from "@/types/listings";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import VotingButtons from "./VotingButtons";
+
+interface ListingViewProps {
+  listing: Listing;
+  userId: string;
+}
+
+const ListingView = ({ listing, userId }: ListingViewProps) => {
+  const [submitted, setSubmitted] = useState(false);
+  const [username, setUsername] = useState("");
+  const [body, setBody] = useState("");
+
+  const { data, isLoading, refetch, error } = useFindComments(listing.id);
+
+  console.log("data", data);
+
+  const createCommentMutation = useCreateComment();
+
+  const handleCreateComment = (
+    comment: CommentData,
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    createCommentMutation
+      .mutateAsync({
+        ...comment,
+        listingId: listing.id,
+      })
+      .then(() => {
+        setSubmitted(true);
+        refetch();
+        setTimeout(() => setSubmitted(false), 3000);
+        refetch();
+      });
+  };
+
   return (
-    <div>
-      <h1>Listing Full View</h1>
-    </div>
+    <>
+      <Box
+        sx={{
+          position: "absolute" as "absolute",
+          display: "flex",
+          direction: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "80%",
+          height: "80%",
+          bgcolor: "white",
+          borderRadius: "20px",
+          lineHeight: "1",
+        }}
+      >
+        <Stack>
+          <VotingButtons listingId={listing.id} userId={userId} />
+          <Typography variant="h1" color={"black"}>
+            {listing.address}
+          </Typography>
+          <Stack>
+            <TextField
+              id="name"
+              label="name"
+              value={username}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setUsername(event.target.value);
+              }}
+            />
+            <TextField
+              multiline
+              rows={4}
+              id="body"
+              label="body"
+              value={body}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setBody(event.target.value);
+              }}
+            />
+            <Button
+              variant="contained"
+              onClick={(e) =>
+                handleCreateComment(
+                  { body: body, name: username, listingId: listing.id },
+                  e
+                )
+              }
+            >
+              Submit
+            </Button>
+          </Stack>
+          <Typography variant="h2" color={"black"}>
+            Comments:
+          </Typography>
+          <Box
+            gap={2}
+            display={"flex"}
+            flexDirection={"column"}
+            alignItems={"flex-start"}
+            justifyContent={"center"}
+            height={"100px"}
+            overflow={"scroll"}
+          >
+            {!isLoading &&
+              data?.map((comment: Comment) => {
+                return (
+                  <Typography variant="body1" color={"black"} key={comment?.id}>
+                    <VotingButtons commentId={comment?.id} userId={userId} />
+                    {comment.body}
+                  </Typography>
+                );
+              })}
+          </Box>
+        </Stack>
+      </Box>
+    </>
   );
 };
 
-export default ListingFullView;
+export default ListingView;
--- src/components/ListingsTable.tsx
+++ src/components/ListingsTable.tsx
@@ -1,22 +1,39 @@
 import { Listing } from "@/types/listings";
 import {
+  Box,
+  Modal,
+  Stack,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
+  useTheme,
 } from "@mui/material";
 import dayjs from "dayjs";
+import { useState } from "react";
+import ListingView from "./ListingView";
+import VotingButtons from "./VotingButtons";
+import parsePrice from "@/utils/parsePrice";
 
 interface ListingsTableProps {
   listings: Listing[];
+  userId: string;
+  total: number;
 }
 
-const ListingsTable = ({ listings }: ListingsTableProps) => {
-  console.log(listings, "this is in fact coming thru");
+const ListingsTable = ({ listings, userId, total }: ListingsTableProps) => {
+  const [open, setOpen] = useState(false);
+  const [rows, setRows] = useState(10);
+  const [page, setPage] = useState(0);
+  const [selectedListing, setSelectedListing] = useState<Listing | null>(null);
+
+  const theme = useTheme();
 
   const columnHeaders = [
+    { field: "voteButtons", headerName: "Votes" },
     {
       field: "fullAddress",
       headerName: "Address",
@@ -39,33 +56,97 @@ const ListingsTable = ({ listings }: ListingsTableProps) => {
     },
   ];
 
+  const handleRowClick = (listing: Listing) => {
+    setSelectedListing(listing);
+    setOpen(true);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRows(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {columnHeaders.map((column) => (
-              <TableCell key={column.field}>{column.headerName}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {listings?.map((listing) => {
-            return (
-              <TableRow key={listing.id}>
-                <TableCell>{`${listing?.address} ${listing?.city} ${listing?.state} ${listing?.zip}`}</TableCell>
-                <TableCell>{listing?.price}</TableCell>
-                <TableCell>{`${listing?.beds} Beds/${listing?.baths} Baths`}</TableCell>
-                <TableCell>{listing?.squareFeet + "Sq. Ft."}</TableCell>
-                <TableCell>
-                  {dayjs(listing?.soldDate).format("DD/MM/YYYY")}
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        borderTop={`1px solid ${theme.palette.divider}`}
+      >
+        <TableContainer>
+          <Table>
+            <TableBody>
+              {listings?.map((listing) => {
+                return (
+                  <TableRow
+                    hover
+                    key={listing.id}
+                    sx={{
+                      border: 0,
+                      cursor: "pointer",
+                    }}
+                  >
+                    <TableCell>
+                      <VotingButtons listingId={listing.id} userId={userId} />
+                    </TableCell>
+                    <TableCell
+                      onClick={() => handleRowClick(listing)}
+                    >{`${listing?.address} ${listing?.city} ${listing?.state} ${listing?.zip}`}</TableCell>
+                    <TableCell onClick={() => handleRowClick(listing)}>
+                      {parsePrice(listing?.price)}
+                    </TableCell>
+                    <TableCell
+                      onClick={() => handleRowClick(listing)}
+                    >{`${listing?.beds}/${listing?.baths}`}</TableCell>
+                    <TableCell onClick={() => handleRowClick(listing)}>
+                      {listing?.squareFeet > 0
+                        ? listing?.squareFeet + " " + "Sq. Ft."
+                        : "--"}
+                    </TableCell>
+                    <TableCell onClick={() => handleRowClick(listing)}>
+                      {dayjs(listing?.soldDate).format("MM/DD/YYYY")}
+                    </TableCell>
+                  </TableRow>
+                );
+              }, [])}
+            </TableBody>
+            <TablePagination
+              count={total}
+              rowsPerPageOptions={[10, 25, 50]}
+              SelectProps={{
+                inputProps: {
+                  "aria-label": "rows per page",
+                },
+                native: true,
+              }}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              rowsPerPage={rows}
+              page={page}
+              onPageChange={handleChangePage}
+            />
+          </Table>
+        </TableContainer>
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+          aria-labelledby="parent-modal-title"
+          aria-describedby="parent-modal-description"
+        >
+          <>
+            {selectedListing && (
+              <ListingView listing={selectedListing} userId={userId} />
+            )}
+          </>
+        </Modal>
+      </Box>
+    </>
   );
 };
 
